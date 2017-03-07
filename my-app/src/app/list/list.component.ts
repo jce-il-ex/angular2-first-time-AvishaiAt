a907901b6311e70eb267eea184f31f77ed662388
Attia <avishai.attia@intel.com>
2017-03-07 13:17:18 +0200

    test

---
+++ my-app/src/app/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+nyList = [
+  {
+    id: 1
+  },
+   {
+    id: 2
+  },
+   {
+    id: 3
+  },
+   {
+    id: 4
+  }
+]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
